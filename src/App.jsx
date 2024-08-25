f36ac2b3af51a80c7e7231814df36e90adeca15d
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-08-25 23:34:30 +0600

    add and delete coffee method added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
+import CoffeeCard from './components/CoffeeCard';
 
 function App() {
 
@@ -7,10 +8,14 @@ function App() {
   console.log(coffees)
   return (
     <>
-      <h1 className='text-2xl text-purple-700'>A Coffee Store</h1>
-      <h1 className='text-xl text-purple-900'>There are Available Coffee: {coffees.length}</h1>
-      <div className='grid md:grid-cols-2 gap-2'>
-
+      <div className="text-center mb-5">
+        <h1 className='text-2xl text-purple-700'>A Coffee Store</h1>
+        <h1 className='text-xl text-purple-900'>There are Available Coffee: {coffees.length}</h1>
+      </div>
+      <div className='grid md:grid-cols-2 md:w-9/12 mx-auto gap-4'>
+        {
+          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+        }
       </div>
     </>
   )
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,9 +1,68 @@
+/* eslint-disable react/prop-types */
+
+import Swal from "sweetalert2";
 
 
-const CoffeeCard = () => {
+const CoffeeCard = ({ coffee }) => {
+    const { _id, name, supplier, taste, category, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
-        <div>
-
+        <div className="grid md:grid-cols-3 items-center p-3 bg-[#F5F4F1] rounded-xl">
+            <div>
+                <figure>
+                    <img src={photo} alt="Coffee" />
+                </figure>
+            </div>
+            <div className="">
+                <p className="text-left">
+                    <span className="font-bold">Name: </span>{name}
+                </p>
+                <p className="text-left">
+                    <span className="font-bold">Taste: </span>{taste}
+                </p>
+                <p className="text-left">
+                    <span className="font-bold">Category: </span>{category}
+                </p>
+                <p className="text-left">
+                    <span className="font-bold">Supplier: </span>{supplier}
+                </p>
+            </div>
+            <div className="flex flex-col mx-auto my-auto gap-2 me-0">
+                <button className="btn btn-warning">View</button>
+                <button className="btn btn-neutral">Edit</button>
+                <button className="btn btn-error"
+                    onClick={() => handleDelete(_id)}
+                >Delete</button>
+            </div>
         </div>
     );
 };
